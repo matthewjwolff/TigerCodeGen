8565ae229d26eab9d2f27c9133df70045d961e9e
Matthew Wolff <mwolff3@lsu.edu>
2016-04-30 01:09:44 -0500

    change

--- testcases/test1.s
+++ testcases/test1.s
@@ -1,64 +1,101 @@
-PROCEDURE tigermain
-# Before canonicalization: 
-MOVE(
- TEMP $v0,
- ESEQ(
-  MOVE(
-   TEMP t33,
-   CALL(
-    NAME _initArray,
-     CONST 10,
-     CONST 0)),
-  TEMP t33))
-# After canonicalization: 
-MOVE(
- TEMP t33,
- CALL(
-  NAME _initArray,
-   CONST 10,
-   CONST 0))
-MOVE(
- TEMP $v0,
- TEMP t33)
-# Basic Blocks: 
-#
-LABEL L1
-MOVE(
- TEMP t33,
- CALL(
-  NAME _initArray,
-   CONST 10,
-   CONST 0))
-MOVE(
- TEMP $v0,
- TEMP t33)
-JUMP(
- NAME L0)
-LABEL L0
-# Trace Scheduled: 
-LABEL L1
-MOVE(
- TEMP t33,
- CALL(
-  NAME _initArray,
-   CONST 10,
-   CONST 0))
-MOVE(
- TEMP $v0,
- TEMP t33)
-JUMP(
- NAME L0)
-LABEL L0
-# Instructions: 
+	.text
+tigermain:
+tigermain_framesize=44
+	subu $sp tigermain_framesize
 L1:
-	li t34 10
-	move $a0 t34
+	addu $v0 $sp tigermain_framesize
+	li $v0 -4
+	add $v0 $v0
+	sw ($v0) $ra
+	addu $v0 $sp tigermain_framesize
+	li $v0 -8
+	add $v0 $v0
+	sw ($v0) $s0
+	addu $v0 $sp tigermain_framesize
+	li $v0 -12
+	add $v0 $v0
+	sw ($v0) $s1
+	addu $v0 $sp tigermain_framesize
+	li $v0 -16
+	add $v0 $v0
+	sw ($v0) $s2
+	addu $v0 $sp tigermain_framesize
+	li $v0 -20
+	add $v0 $v0
+	sw ($v0) $s3
+	addu $v0 $sp tigermain_framesize
+	li $v0 -24
+	add $v0 $v0
+	sw ($v0) $s4
+	addu $v0 $sp tigermain_framesize
+	li $v0 -28
+	add $v0 $v0
+	sw ($v0) $s5
+	addu $v0 $sp tigermain_framesize
+	li $v0 -32
+	add $v0 $v0
+	sw ($v0) $s6
+	addu $v0 $sp tigermain_framesize
+	li $v0 -36
+	add $v0 $v0
+	sw ($v0) $s7
+	addu $v0 $sp tigermain_framesize
+	li $v0 -40
+	add $v0 $v0
+	sw ($v0) $s8
+	addu $v0 $sp tigermain_framesize
+	add $v0 $0
+	sw ($v0) $a0
+	li $v0 10
+	move $a0 $v0
 	move $a1 $0
 	jal _initArray
-	lw t33 $v0
-	lw $v0 t33
+	lw $v0 $v0
+	lw $v0 $v0
+	addu $v1 $sp tigermain_framesize
+	li $v1 -40
+	add $v1 $v1
+	lw $s8 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -36
+	add $v1 $v1
+	lw $s7 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -32
+	add $v1 $v1
+	lw $s6 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -28
+	add $v1 $v1
+	lw $s5 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -24
+	add $v1 $v1
+	lw $s4 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -20
+	add $v1 $v1
+	lw $s3 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -16
+	add $v1 $v1
+	lw $s2 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -12
+	add $v1 $v1
+	lw $s1 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -8
+	add $v1 $v1
+	lw $s0 ($v1)
+	addu $v1 $sp tigermain_framesize
+	li $v1 -4
+	add $v1 $v1
+	lw $ra ($v1)
 	b L0
-	la t35 L0
-	jr t35
+	la $v1 L0
+	jr $v1
 L0:
-END tigermain
+
+	addu $sp tigermain_framesize
+	j $ra
